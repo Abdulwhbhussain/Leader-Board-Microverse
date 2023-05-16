1da8707e3bab4536b3c606ef1ea7d83e2a6ec04f
WHB901 <abdulwhbhussain@gmail.com>
2023-05-16 15:22:13 +0500

    Setup webpack, npm, linters, file structure

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
